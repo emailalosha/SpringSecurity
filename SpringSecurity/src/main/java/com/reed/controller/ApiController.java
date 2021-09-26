195bf419d883077bae0e322baa4a87a394bf4353
Alok Kumar <al.kumar@hotmail.com>
2021-09-26 10:34:18 +0530

    final code

--- SpringSecurity/src/main/java/com/reed/controller/ApiController.java
+++ SpringSecurity/src/main/java/com/reed/controller/ApiController.java
@@ -1,6 +1,18 @@
 package com.reed.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApiController
 {
 
+	@GetMapping("/welcome")
+	public ResponseEntity<Object> hello()
+	{
+		return ResponseEntity.status(HttpStatus.OK).body("Hello to All.....");
+	}
+	
 }
